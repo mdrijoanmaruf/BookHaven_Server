9631ccfcaea57a782094f9eda0642d077569c768
Md Rijoan Maruf <rmsaif4430@gmail.com>
2025-06-17 18:10:45 +0600

    Fix middlewate problem

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://bookhaven.rijoan.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true
+}));
 app.use(express.json());
 
 
